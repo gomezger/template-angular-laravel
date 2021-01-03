3261b3354b8d77985739f075e9cf58d3b1966adc
Germán A. Gómez <germang08@hotmail.com>
2021-01-03 11:49:02 -0300

    Sacar jQuery del toggled del panel

--- client/src/app/components/panel/panel.component.ts
+++ client/src/app/components/panel/panel.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import * as $ from 'jquery';
 import { UserService } from '../../services/users/user.service';
 import { Router, ActivatedRoute } from '@angular/router';
 
@@ -11,18 +10,21 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class PanelComponent implements OnInit {
 
   constructor(
-    private _usuario: UserService, 
-    private _router: Router, 
+    private _usuario: UserService,
+    private _router: Router,
     private _route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
   }
-  
+
 	toggle() {
-		$('#wrapper').toggleClass('toggled');
+    const menuDiv = document.getElementById('wrapper');
+    (menuDiv.classList.contains('toggled'))
+      ? menuDiv.classList.remove('toggled')
+      : menuDiv.classList.add('toggled');
   }
-  
+
 	logout() {
 		this._usuario.logout();
 		this._router.navigate([ '/panel/login' ], { relativeTo: this._route });
